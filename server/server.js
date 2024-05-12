3936d542e5187680114a56d67e6d6920a0956ed4
Parth <ppj8988@gmail.com>
2024-05-12 14:45:32 +0530

    Initialized Server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log("Error: "+err);
+    }else{
+        console.log(`Server is running on http://localhost:${port}`);
+    }
+})
