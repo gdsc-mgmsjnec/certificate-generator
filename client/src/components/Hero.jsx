01516e7a883766c5a96322f20ea1c1d006ec0184
Parth <ppj8988@gmail.com>
2024-05-12 16:03:11 +0530

    Connection Established between Front End and Backend

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Hero.css"
+import axios from 'axios';
+
+const API_key = "Pj_$jammy_techTeam_GDSC2024";
+
 
 function Hero(){
+    const [inutEmail, getEmail] = useState("")
+
+    function handleChange(event){
+        getEmail(event.target.value)
+    }
+
+    async function handleSubmit(event){
+        event.preventDefault();
+
+        const result = await axios.get("http://localhost:3000/",{
+            params:{
+                "email":inutEmail
+            }
+        });
+        alert(result.data.email);
+    }
+
     return <main>
         <div>
             <h1>Google Developers Student Club</h1>
             <h2>MGM's Jawaharlal Nehru Engineering College, Aurangabad</h2>
         </div>
-        <form>
-            <input type="email" name="email" id="email" placeholder="Enter your Email..." />
-            <button>Submit</button>
+        <form onSubmit={handleSubmit}>
+            <input onChange={handleChange} type="email" name="email" id="email" value={inutEmail} placeholder="Enter your Email..." />
+            <button type='submit'>Submit</button>
         </form>
     </main>
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors())
+
+app.get("/", async(req, res) =>{
+    const data = req.query.email;
+    console.log(data)
+    res.json({"email": data});
+})
 
 app.listen(port, (err)=>{
     if(err){
