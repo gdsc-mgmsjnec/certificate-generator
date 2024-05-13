403391aa3e0240b1585457069b79f12920e0ad89
Parth <ppj8988@gmail.com>
2024-05-13 15:46:58 +0530

    Bug Fixed

--- client/src/components/Certificate.jsx
+++ client/src/components/Certificate.jsx
@@ -28,15 +28,13 @@ function Certificate(props){
         props.goBack();
     }
 
-    const customStyle=props.certName.length>20 ? { fontSize: "150px" } : null;
-
     return(<>
         <div className="download-btn">
             <button onClick={downloadPdf}>Download</button>
         </div>
         <div ref={pdfRef}>
             <img style={{width: "100%"}} src="/certificate_template.png" alt="" />
-            <h1 style={customStyle} className="name">{props.certName}</h1>
+            <h1 style={props.certName.length>20 ? { fontSize: "150px" } : {fontSize: "190px"}} className="name">{props.certName}</h1>
         </div>
         </>
     )
